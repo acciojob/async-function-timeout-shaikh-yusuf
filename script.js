af840bbab41e732d07738629ffb0c9bb44fcff06
shaikh-yusuf <94888066+shaikh-yusuf@users.noreply.github.com>
2024-01-24 11:50:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+document.getElementById("btn").addEventListener("click", async()=> {
+  let text=document.getElementById("Text")
+	let delay=document.getElementById("Delay")
+	if(text && delay)
+	{
+		let myPromise=new Promise((res)=>{
+			setTimeout(()=>{
+				res(text)
+			},delay)
+		})
+		let text2=await myPromise;
+		if(text2)
+		{
+			document.getElementById('output').textContent=text2
+		}
+	}
+});
